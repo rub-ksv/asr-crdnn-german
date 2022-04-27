c30ccf2349db80ed170602695e1ea76a34049bab
jf-rub-ksv <84972666+jf-rub-ksv@users.noreply.github.com>
2022-04-27 13:54:44 +0200

    Update just_use_the_model.py

--- just_use_the_model.py
+++ just_use_the_model.py
@@ -3,3 +3,4 @@ from speechbrain.pretrained import EncoderDecoderASR
 asr_model = EncoderDecoderASR.from_hparams(source="jfreiwa/asr-crdnn-german", savedir="pretrained_models/asr-crdnn-german")
 transcript = asr_model.transcribe_file("jfreiwa/asr-crdnn-german/example-de.wav")
 print(transcript)
+# >> hier tragen die mitgliedstaaten eine entsprechend grosse verantwortung
