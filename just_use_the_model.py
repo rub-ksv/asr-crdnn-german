4234cc2c1c4e511e2e7f62ecc1121b89b5162f54
jf-rub-ksv <84972666+jf-rub-ksv@users.noreply.github.com>
2022-04-27 13:45:59 +0200

    Update just_use_the_model.py

--- just_use_the_model.py
+++ just_use_the_model.py
@@ -1,4 +1,5 @@
 from speechbrain.pretrained import EncoderDecoderASR
 
 asr_model = EncoderDecoderASR.from_hparams(source="jfreiwa/asr-crdnn-german", savedir="pretrained_models/asr-crdnn-german")
-asr_model.transcribe_file("jfreiwa/asr-crdnn-german/example-de.wav")
+transcript = asr_model.transcribe_file("jfreiwa/asr-crdnn-german/example-de.wav")
+print(transcript)
